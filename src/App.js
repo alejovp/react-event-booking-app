4f3b272ba6ac1f7ec2e7a0d4d67cf6b8a156a893
alejovp <alejovp@gmail.com>
2019-03-03 19:45:37 +0100

    React custom boilerplate project with webpack, babel and sass loaders

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const App = () => (
+    <div>
+        <h1>This is my basic react boilerplate project!!</h1>
+    </div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'public');
+
+
+module.exports = {
+	entry: {
+		main: ['babel-polyfill', `${APP_DIR}/index.js`]
+	},
+	devServer: {
+		contentBase: BUILD_DIR,
+		publicPath: '/js/'
+	},
+	output: {
+		filename: '[name].bundle.js',
+		// sourceMapFilename: '[name].bundle.js.map',
+		path: `${BUILD_DIR}/js`
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(scss)$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				],
+			},
+			{
+				test: /\.jsx?$/,
+				include: [
+					APP_DIR,
+				],
+				loader: 'babel-loader'
+			},
+		]
+	},
+};
