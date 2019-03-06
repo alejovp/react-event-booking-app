3a93945747c9b0a0df48fca80fd1adeedc0d820d
alejovp <alejovp@gmail.com>
2019-03-06 16:21:59 +0100

    First Navbar created with basic navigation

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { AuthView } from './views/auth';
 import { EventsView } from './views/events';
 import { BookingsView } from './views/bookings';
+import Navbar from './components/Navbar';
 
 
 export const App = () => (
     <BrowserRouter>
-        <Switch>
-            <Redirect from="/" to="/auth" exact />
-            <Route path="/auth" component={AuthView} />
-            <Route path="/events" component={EventsView} />
-            <Route path="/bookings" component={BookingsView} />
-        </Switch>
+        <React.Fragment>
+            <Navbar />
+            <main>
+                <Switch>
+                    <Redirect from="/" to="/auth" exact />
+                    <Route path="/auth" component={AuthView} />
+                    <Route path="/events" component={EventsView} />
+                    <Route path="/bookings" component={BookingsView} />
+                </Switch>
+            </main>
+        </React.Fragment>
     </BrowserRouter>
 );
