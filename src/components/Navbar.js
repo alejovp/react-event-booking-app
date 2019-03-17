17c11ded5f9d101378eb2751a0c1eee97332dbd5
alejovp <alejovp@gmail.com>
2019-03-17 18:04:37 +0100

    Now NavBar component as a AuthContext consumer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,65 +8,74 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import AuthContext from '../context/auth-context';
 
 
 const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+	root: {
+		flexGrow: 1,
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+	},
 };
 
-const Navbar = (props) => {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton 
-            className={classes.menuButton} 
-            color="inherit" 
-            aria-label="Menu">
-                <MenuIcon />
-          </IconButton>
-          <Typography 
-            variant="h6" 
-            color="inherit" 
-            className={classes.grow}>
-                EBookings
-          </Typography>
-          <Button
-            component={NavLink}
-            to="/events"
-            color="inherit">
-                Events
-          </Button>
-          <Button
-            component={NavLink}
-            to="/bookings"
-            color="inherit">
-                Bookings
-          </Button>
-          <Button
-            component={NavLink}
-            to="/auth"
-            color="inherit">
-                Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+const Navbar = ({ classes }) => {
+
+	const renderNavBar = context => (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton
+						className={classes.menuButton}
+						color="inherit"
+						aria-label="Menu">
+							<MenuIcon />
+					</IconButton>
+					<Typography
+						variant="h6"
+						color="inherit"
+						className={classes.grow}>
+							EBookings
+					</Typography>
+					<Button
+						component={NavLink}
+						to="/events"
+						color="inherit">
+							Events
+					</Button>
+					{ context.token && 
+						<Button
+							component={NavLink}
+							to="/bookings"
+							color="inherit">
+								Bookings
+						</Button>}
+					{ !context.token && 
+						<Button
+							component={NavLink}
+							to="/auth"
+							color="inherit">
+								Login
+						</Button>}
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+
+	return (
+		<AuthContext.Consumer>
+			{ context => renderNavBar(context) }
+		</AuthContext.Consumer>
+	);
 };
 
 Navbar.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Navbar);
