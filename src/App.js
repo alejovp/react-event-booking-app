e0c707b5df3315ffead40ad0e7e1650e5b9fecc1
alejovp <alejovp@gmail.com>
2019-03-17 17:15:05 +0100

    New auth-context created and added at App level

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import AuthView from './views/AuthView';
 import { EventsView } from './views/EventsView';
 import { BookingsView } from './views/BookingsView';
 import Navbar from './components/Navbar';
+import AuthContext from './context/auth-context';
 
 
-export const App = () => (
-    <BrowserRouter>
-        <React.Fragment>
-            <Navbar />
-            <main>
-                <Switch>
-                    <Redirect from="/" to="/auth" exact />
-                    <Route path="/auth" component={AuthView} />
-                    <Route path="/events" component={EventsView} />
-                    <Route path="/bookings" component={BookingsView} />
-                </Switch>
-            </main>
-        </React.Fragment>
-    </BrowserRouter>
-);
+export class App extends Component {
+
+    render () {
+        return (
+            <BrowserRouter>
+                <React.Fragment>
+                    <AuthContext.Provider>
+                        <Navbar />
+                        <main>
+                            <Switch>
+                                <Redirect from="/" to="/auth" exact />
+                                <Route path="/auth" component={AuthView} />
+                                <Route path="/events" component={EventsView} />
+                                <Route path="/bookings" component={BookingsView} />
+                            </Switch>
+                        </main>
+                    </AuthContext.Provider>
+                </React.Fragment>
+            </BrowserRouter>
+        );
+    }
+}
