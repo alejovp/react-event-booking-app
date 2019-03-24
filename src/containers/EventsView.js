eab547941071890067621b56b9c6161c251598bf
alejovp <alejovp@gmail.com>
2019-03-24 21:19:13 +0100

    EventsView now using withModal hoc

--- src/containers/EventsView.js
+++ src/containers/EventsView.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { withModal } from '../hocs/withModal';
 
 
-export class EventsView extends Component {
+class EventsView extends Component {
+
+    onShowModal = () => this.props.onShowModal('Modal Title', <p>Modal Content</p>);
+    
     render() {
         return (
             <div>
                 <h1>This is the events view!</h1>
+                <button onClick={this.onShowModal}>Open Modal</button>
             </div>
         );
     }
 }
+
+export default withModal(EventsView);
