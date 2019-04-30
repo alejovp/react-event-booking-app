a1dc9999138414372191528ffc7d4d9674c93277
alejovp <alejovp@gmail.com>
2019-04-30 07:36:10 +0200

    EventView now passing the EventForm to the modal comp

--- src/containers/EventsView.js
+++ src/containers/EventsView.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 import { withStyles } from '@material-ui/core/styles';
 import { EventsService } from '../services/EventsService';
 import { withModal } from '../hocs/withModal';
 import Event from '../components/Event/Event';
+import EventForm from '../components/EventForm.js/EventForm';
 
 
 const styles = {
     container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      padding: 16
+        display: 'flex',
+        flexWrap: 'wrap',
+        padding: 16,
+        width: '80vw',
+        margin: '0 auto',
     },
+    fab: {
+        position: 'fixed',
+        bottom: 20,
+        right: 20,
+    }
 };
 
 class EventsView extends Component {
@@ -32,15 +42,27 @@ class EventsView extends Component {
 
     setEvents = events => this.setState({ events });
 
-    onShowModal = () => this.props.onShowModal('Modal Title', <p>Modal Content</p>);
+    onShowModal = () => this.props.onShowModal(
+        'New Event', 
+        <EventForm 
+            onSubmit={this.onSubmit}
+            onCancel={this.props.onCloseModal}
+        />
+    );
+
+    onSubmit = params => {
+        console.log(params);
+        this.props.onCloseModal()
+    }
 
     renderEvents = () => {
         return this.state.events.map(event => (
             <Grid 
                 item 
                 xs={12}
-                sm={4}
-                md={3}
+                sm={6}
+                md={4}
+                lg={3}
                 key={event._id}
             >
                 <Event
@@ -60,7 +82,13 @@ class EventsView extends Component {
         return (    
             <div>
                 <h1>This is the events view!</h1>
-                <button onClick={this.onShowModal}>Open Modal</button>
+                <Fab 
+                    className={classes.fab}
+                    onClick={this.onShowModal}
+                    color="primary"
+                >
+                    <AddIcon />
+                </Fab>
                 <div className={classes.container}>
                     <Grid container spacing={16}>
                         { this.renderEvents() }
