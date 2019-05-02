5e3ee5f4f94b1a22456d5b42d07ef16996e127db
alejovp <alejovp@gmail.com>
2019-05-02 17:43:08 +0200

    Jest installed and configured

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    collectCoverageFrom: [
+        'src/**/*.js',
+        'src/*.js',
+        'src/**/*.jsx',
+        'src/*.jsx'
+    ],
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+        global: {
+            'branches': 0,
+            'functions': 0,
+            'lines': 0,
+            'statements': 0
+        }
+    },
+    globals: {
+        NODE_ENV: 'test'
+    },
+    moduleDirectories: [
+        'node_modules',
+        'src'
+    ],
+    modulePathIgnorePatterns: [
+        '<rootDir>/public',
+    ],
+    testURL: 'http://localhost:8080',
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest'
+    }
+};
